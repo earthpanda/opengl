3e007905ec925d94b5264fcf2919d89a621f0157
yl <earthpanda@foxmail.com>
2020-06-28 20:58:19 +0800

    feature: draw a rect & trangle

--- openglStudy/Main.cpp
+++ openglStudy/Main.cpp
@@ -148,11 +148,11 @@ int main() {
 		//lt
 		-1.0f,1.0f,0.0f,
 		//lb
-		-0.1f,-0.1f,0.0f,
+		-1.0f,-0.0f,0.0f,
 		//rb
-		0.1f,-0.1f,0.0f,
+		0.0f,0.0f,0.0f,
 		//rt
-		0.1f,0.1f,0.0f
+		0.0f,1.0f,0.0f
 	};
 
 	unsigned int indices[] = {
@@ -163,50 +163,77 @@ int main() {
 
 	};
 
-	//float trangle[] = {
+	//三角形
+	float trangle[] = {
+		//l
+		0.0f,0.0f,0.0f,
+		//r
+		1.0f,0.0f,0.0f,
+		//t
+		1.0f,1.0f,0.0f
 
-
-
-
-	//};
+	};
 
 
 	/****************************************************使用IBO 进行矩形的绘制**************************************************************************/
 
 
 	//定义VAO 和 VBO 数组
-	unsigned int VAOS[2], VBOS[2];
+	//矩形使用VAO VBO和IBO 三角形使用VAO和VBO实现
+	unsigned int VAOS[2], VBOS[2],IBO;
+	glGenVertexArrays(2, VAOS);
+	glGenBuffers(2, VBOS);
+	glGenBuffers(1, &IBO);
+
+	//绑定矩形
+	glBindVertexArray(VAOS[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOS[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//绑定三角形
+	glBindVertexArray(VAOS[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOS[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(trangle), trangle, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+/***********************************************************不使用数组进行关于三角形的绘制***************************************************************************/
 
 	//创建顶点数组对象 和 顶点缓冲对象 和  IBO(根据需要进行创建)
 	//其中VAO和VBO的过程是必须的 IBO是可选的
 	//如果使用IBO 则最后的绘制方式有所区别
-	unsigned int VAO, VBO,IBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &IBO);
-	
-	// 1. 绑定顶点数组对象
-	//对于VAO和VBO进行绑定  首先需要对于VAO进行绑定 然后对VBO进行绑定
-	glBindVertexArray(VAO);
+	//unsigned int VAO, VBO,IBO;
+	//glGenVertexArrays(1, &VAO);
+	//glGenBuffers(1, &VBO);
+	//glGenBuffers(1, &IBO);
+	//
+	//// 1. 绑定顶点数组对象
+	////对于VAO和VBO进行绑定  首先需要对于VAO进行绑定 然后对VBO进行绑定
+	//glBindVertexArray(VAO);
 
-	// 2. 把我们的顶点数组复制到一个顶点缓冲中，供OpenGL使用
-	//使用VBO的时候需要创建关于VBO的buffer和绑定数据
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	//对于缓冲进行数据填充
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//// 2. 把我们的顶点数组复制到一个顶点缓冲中，供OpenGL使用
+	////使用VBO的时候需要创建关于VBO的buffer和绑定数据
+	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	////对于缓冲进行数据填充
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// 3. 复制我们的索引数组到一个索引缓冲中，供OpenGL使用
-	//使用IBO进行创建和绘制
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-	//使用IBO进行数据缓冲填充
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//// 3. 复制我们的索引数组到一个索引缓冲中，供OpenGL使用
+	////使用IBO进行创建和绘制
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	////使用IBO进行数据缓冲填充
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	// 4. 设定顶点属性指针
-	//对于顶点数据格式进行解释
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
-	glEnableVertexAttribArray(0);
+	//// 4. 设定顶点属性指针
+	////对于顶点数据格式进行解释
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+	//glEnableVertexAttribArray(0);
 
-
+	/**********************************************************************************************************************************************/
 
 
 
@@ -231,8 +258,6 @@ int main() {
 
 		//绘制三角形
 		glUseProgram(shaderProgram);
-		// seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glBindVertexArray(VAO); 
 		//这里决定画多少个顶点
 		//如果指定3 则会绘制前三个顶点
 		//如果使用了ibo 则不用这样的绘制方式
@@ -245,9 +270,15 @@ int main() {
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 
-		//使用IBO来进行绘制
+		//使用IBO来进行绘制矩形
+		glBindVertexArray(VAOS[0]);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		//使用VBO来进行三角形的绘制
+		glBindVertexArray(VAOS[1]);
+		glBindBuffer(GL_ARRAY_BUFFER, VBOS[1]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
 
@@ -261,8 +292,12 @@ int main() {
 
 	// 进行资源的释放
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	/*glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);*/
+
+
+	glDeleteVertexArrays(2, VAOS);
+	glDeleteBuffers(2, VBOS);
 	glDeleteBuffers(1, &IBO);
 	glDeleteProgram(shaderProgram);
 
